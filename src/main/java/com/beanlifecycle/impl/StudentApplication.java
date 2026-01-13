06d45d8c7be573e0523bd079c8ce487bc6e6cd20
Manis09 <bhawsar9159@gmail.com>
2026-01-13 14:39:26 +0530

    Bean life cycle with implementing interface

--- src/main/java/com/beanlifecycle/impl/StudentApplication.java
+++ src/main/java/com/beanlifecycle/impl/StudentApplication.java
@@ -1,16 +1,13 @@
-package beanlifecycle.xmlbased;
+package com.beanlifecycle.impl;
 
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class StudentApp {
+public class StudentApplication {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context =new ClassPathXmlApplicationContext("beanlccfg.xml");
-
+        AbstractApplicationContext context=new ClassPathXmlApplicationContext("implcfg.xml");
+        context.registerShutdownHook();
         Student stu = context.getBean("stu", Student.class);
         System.out.println(stu);
-        context.close();
     }
 }
-
